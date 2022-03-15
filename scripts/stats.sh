1a94e247b6a4589f713ea6d6ae05541d14cec318
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2022-03-15 18:25:08 -0400

    Allow scripts/stats.sh to read from stdin

--- scripts/stats.sh
+++ scripts/stats.sh
@@ -17,17 +17,45 @@ function delete_lock {
 	rm -f .tmp/lock_stats
 }
 
+declare -a _dirs
+
+_STDIN=0
+while [[ $# -gt 0 ]]
+do
+	_arg="$1"; shift
+	case "${_arg}" in
+		--stdin) _STDIN=1
+		echo "stdin = [${_STDIN}]"
+		;;
+		--) break ;;
+		-h | --help)
+		>&2 echo "Options for $(basename "$0") are:"
+		>&2 echo "--stdin force read directories from stdin instead of script's arguments"
+		exit 1
+		;;
+		*) _dirs+=("${_arg}") ;;
+	esac
+done
+
 acquire_lock || exit 1
 
 trap delete_lock EXIT
 
+if [[ ${#_dirs[@]} == 0 ]] || [[ ${_STDIN} == 1 ]]
+then
+	readarray -t _dirs
+fi
+
 rm -f files_count.stats
-for dir in "$@"
+for dir in "${_dirs[@]}"
 do
 	echo $(find $dir -type f | wc -l; echo $dir) >> files_count.stats
 done
 
 rm -f disk_usage.stats
-du -s "$@" > disk_usage.stats
+for d in "${_dirs[@]}" ; do printf "%s\0" "${d}" ; done | du -s --files0-from - > disk_usage.stats
 
-chmod -R a-w "$@"
+for d in "${_dirs[@]}"
+do
+	chmod -R a-w "$d"
+done
